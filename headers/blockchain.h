ecbc80c122a2cc8223d01ddd951ccdcc6c14e213
Thulio Mattheus Pereira de Sousa <thulio.sousa@dataprev.gov.br>
2018-10-17 15:53:32 -0300

    Refatoração dos nomes dos arquivos e utilização de json para armazenar os dados dos blocos

--- headers/blockchain.h
+++ headers/blockchain.h
@@ -1,30 +1,34 @@
 #ifndef urn_h
 #define urn_h
 
-#include "voteBlock.h"
-#include "stringUtil.h"
+#include "block.h"
+
+#include "utilities.h"
+    using Utilities::StringUtil;
 
 #include <vector>
 
 using namespace std;
 
-class Urn{
+class Blockchain{
 
     private:
         const string genesisPreviousHash = "0000000000000000000000000000000000000000000000000000000000000000";
 
-        void createGenesisBlock(string data);
+        void createGenesisBlock(Document& data);
 
     public:
-        Urn();
+        Blockchain();
+        Blockchain operator=(Blockchain b);
 
         int deep;
-        vector<VoteBlock> chain;
+        vector<Block> chain;
         const static int difficulty = 5;
 
-        void addBlock(string data);
+        void addBlock(Document& data);
         void showBlocks();
         bool isChainValid();
+        void proofOfWork(int blockIndex, int difficulty);
 
 };
 
--- headers/wallet.h
+++ headers/wallet.h
@@ -1,12 +1,14 @@
-#ifndef electorArea_h
-#define electorArea_h
+#ifndef wallet_h
+#define wallet_h
 
 #include <iostream>
     using namespace std;
 
-#include "urn.h"
-#include "vote.h"
-#include "stringUtil.h"
+#include "blockchain.h"
+#include "transaction.h"
+
+#include "utilities.h"
+    using Utilities::StringUtil;
 
 #include "../lib/cryptopp/rsa.h"
     using CryptoPP::RSA;
@@ -14,7 +16,7 @@
 #include "../lib/cryptopp/osrng.h"
     using CryptoPP::AutoSeededRandomPool;
 
-class ElectorArea{
+class Wallet{
 
     private:
 
@@ -23,20 +25,22 @@ class ElectorArea{
         string voterTitle;
     	RSA::PrivateKey privKey;
     	RSA::PublicKey pubKey;
-        Urn blockchain;
+        Blockchain blockchain;
 
         // string getPrivateKeyAsString();
         // string getPublicKeyAsString();
 
     public:
 
-        ElectorArea(string name, string cpf, string voterTitle);
+        Wallet(string name, string cpf, string voterTitle);
 
         string getName();
 
         RSA::PublicKey getPublicKey();
 
-        Vote toVote(RSA::PublicKey candidate);
+        Transaction toVote(RSA::PublicKey candidate);
+
+        void updateBlockchain(Blockchain b);
 
         void teste();
 
