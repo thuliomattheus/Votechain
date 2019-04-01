1cf6dcac619eb92c99f8f4d8901ee6b11e3bd5c0
Thúlio Sousa <thuliomattheus@hotmail.com>
2019-04-01 20:50:50 -0300

    melhorando o script de limpeza do banco

--- blockchainNode/nodeProject/scripts/atualizarBanco.sh
+++ blockchainNode/nodeProject/scripts/atualizarBanco.sh
@@ -2,8 +2,6 @@
 cd ~/Documentos/TCC/blockchainNode
 rm nodeDatabase.sqlite3
 rm -r nodeProject/nodeApp/migrations
-rm -r nodeProject/voteReusableApp/migrations
-python manage.py makemigrations nodeApp voteReusableApp
+python manage.py makemigrations nodeApp
 python manage.py migrate
 cd -
-
